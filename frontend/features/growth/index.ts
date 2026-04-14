6956fb0490edf63bfcb201809aa53fc88c9ea5d0
MannyMeng <manny.mengxb@gmail.com>
2026-04-15 10:34:31 +1200

    feat(growth): export WeightChart and WeightChartTooltip

--- frontend/features/growth/index.ts
+++ frontend/features/growth/index.ts
@@ -1,2 +1,4 @@
 export { default as GrowthForm } from './components/GrowthForm';
+export { default as WeightChart } from './components/WeightChart';
+export { default as WeightChartTooltip } from './components/WeightChartTooltip';
 
